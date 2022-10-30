9c7a21eb3f40935065cce9cca3ae9324aa2f3288
Michael van Niekerk <mike@agri-io.co.za>
2022-10-30 10:16:51 +0200

    Here be segfault

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,23 +1,33 @@
-use rand::{seq::SliceRandom, Rng};
+// use rand::{seq::SliceRandom, Rng};
+
+extern crate core;
+
+use rand::rngs::ThreadRng;
+use rand::seq::SliceRandom;
+use rand::Rng;
 
 // tuneable parameters
 // TODO: extract these to somewhere nicer
-const NUM_POINTS: i32 = 256;
-const MAX_SIZE: i32 = 10; // basically the dimensions of the world
-const MAX_POPULATION: i32 = 1000;
+const NUM_POINTS: usize = 256;
+const MAX_POPULATION: usize = 1000;
 const NUM_GENERATIONS: i32 = 50;
-const NUM_SURVIVORS: i32 = 100;
+const NUM_SURVIVORS: usize = 100;
 
 type Point = (f64, f64);
+type Population = [[usize; NUM_POINTS]; MAX_POPULATION];
+type World = [Point; NUM_POINTS];
 
 fn main() {
     // set up world
-    let world: Vec<Point> = (0..NUM_POINTS).map(|_| new_point()).collect();
+    let world: World = core::array::from_fn(|_| (fastrand::f64(), fastrand::f64()));
 
     let start = std::time::Instant::now();
+    // let mut rng = StepRng::new(2, 13);
+    // let mut rng = StepRng::new(2, 13);
+    let mut rng = rand::thread_rng();
 
     // set up the initial population
-    let mut population: Vec<Vec<i32>> = (0..MAX_POPULATION).map(|_| new_random_agent()).collect();
+    let mut population: Population = core::array::from_fn(|_| new_random_agent(&mut rng));
 
     for _ in 0..NUM_GENERATIONS {
         eval_and_sort(&mut population, &world);
@@ -25,9 +35,7 @@ fn main() {
         // determine the set of survivors
         let survivors = &population[0..NUM_SURVIVORS as usize];
 
-        population = (0..MAX_POPULATION)
-            .map(|_| new_agent_from_parents(survivors))
-            .collect();
+        population = core::array::from_fn(|_| new_agent_from_parents(survivors, &mut rng));
     }
 
     eval_and_sort(&mut population, &world);
@@ -40,25 +48,20 @@ fn main() {
     println!("Elapsed: {:?}", elapsed);
 }
 
-fn new_point() -> Point {
-    let x: f64 = rand::thread_rng().gen_range(0.0..MAX_SIZE as f64);
-    let y: f64 = rand::thread_rng().gen_range(0.0..MAX_SIZE as f64);
-    (x, y)
-}
-
-fn new_random_agent() -> Vec<i32> {
-    let mut agent: Vec<i32> = (0..NUM_POINTS).collect();
-    agent.shuffle(&mut rand::thread_rng());
+fn new_random_agent(rng: &mut ThreadRng) -> [usize; NUM_POINTS]
+{
+    let mut agent: [usize; NUM_POINTS] = core::array::from_fn(|i| i);
+    agent.shuffle(rng);
     agent
 }
 
-fn new_agent_from_parents(parents: &[Vec<i32>]) -> Vec<i32> {
-    let parent = parents.choose(&mut rand::thread_rng()).unwrap();
+fn new_agent_from_parents(parents: &[[usize; NUM_POINTS]], mut rng: &mut ThreadRng) -> [usize; NUM_POINTS] {
+    let parent = parents.choose(&mut rng).unwrap();
 
-    let mut agent: Vec<i32> = parent.clone();
+    let mut agent = *parent;
     agent.swap(
-        rand::thread_rng().gen_range(0..NUM_POINTS) as usize,
-        rand::thread_rng().gen_range(0..NUM_POINTS) as usize,
+        rng.gen_range(0..NUM_POINTS) as usize,
+        rng.gen_range(0..NUM_POINTS) as usize,
     );
     agent
 }
@@ -67,18 +70,18 @@ fn distance(a: &Point, b: &Point) -> f64 {
     ((a.0 - b.0).powi(2) + (a.1 - b.1).powi(2)).sqrt()
 }
 
-fn eval(agent: &[i32], world: &[Point]) -> f64 {
+fn eval(agent: &[usize], world: &World) -> f64 {
     (0..NUM_POINTS)
         .zip((1..NUM_POINTS).chain(0..1))
         .map(|(i, j)| {
             distance(
-                &world[agent[i as usize] as usize],
-                &world[agent[j as usize] as usize],
+                &world[agent[i]],
+                &world[agent[j]],
             )
         })
         .sum()
 }
 
-fn eval_and_sort(pop: &mut [Vec<i32>], world: &[Point]) {
+fn eval_and_sort(pop: &mut Population, world: &World) {
     pop.sort_by(|a, b| eval(a, world).partial_cmp(&eval(b, world)).unwrap());
 }
